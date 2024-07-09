d1479cd7347eec440c5a57d1a3993bcca6f96ae5
Victor Fonseca <victortales@gmail.com>
2024-07-09 09:47:16 +0000

    Initial Notebook trainning and data setup is ready.

--- mix.exs
+++ mix.exs
@@ -22,12 +22,14 @@ defmodule ElixirMlLearning.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nx, "~> 0.5"},
+      {:nx, "~> 0.7"},
       {:scidata, "~> 0.1"},
-      {:axon, "~> 0.5"},
-      {:explorer, "~> 0.5"},
-      {:kino, "~> 0.9"},
-      {:vega_lite, "~> 0.1"}
+      {:axon, "~> 0.6"},
+      {:explorer, "~> 0.8"},
+      {:kino, "~> 0.13"},
+      {:vega_lite, "~> 0.1"},
+      {:decimal, "~> 2.0"},
+      {:arangox, "~> 0.5.0"}
     ]
   end
 end
